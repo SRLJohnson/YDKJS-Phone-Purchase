24f99b0cbda26b9ab2bead27facd252402b7e509
Sarah Johnson <sarahrljohnson@gmail.com>
2018-11-23 18:38:59 +0100

    added purchaseItem function

--- phone_purchase.js
+++ phone_purchase.js
@@ -1,15 +1,32 @@
 const taxRate = .06;
-const phonePrice = 250.00;
-const accessoryPrice = 24.00;
-const spendingThreshold = 350.00;//input("What is your phone and accessories budget?");
-const bankBalance = 800;
+const phone = 250.00;
+const accessory = 24.00;
+let spendingThreshold = 350.00;//input("What is your phone and accessories budget?");
+let bankBalance = 800;
 
-const withTax = function(itemPrice, taxRate) {
-    let taxAmount = itemPrice*taxRate;
+const withTax = function(itemPrice) {
+    let taxAmount = itemPrice * taxRate;
     let afterTaxes = itemPrice + taxAmount;
     return afterTaxes.toFixed(2);
 }
 
+const purchaseItem = function(item) {
+  bankBalance = bankBalance - withTax(item);
+  spendingThreshold = spendingThreshold - withTax(item);
+}
+
+const purchaseQuantity = function() {
+  while (bankBalance > 0) {
+
+  }
+}
+
 const totalPrice = function() {};
 
-console.log(withTax(phonePrice, taxRate));
+console.log(withTax(phone));
+
+purchaseItem(phone);
+purchaseItem(accessory);
+
+console.log(bankBalance);
+console.log(spendingThreshold);
