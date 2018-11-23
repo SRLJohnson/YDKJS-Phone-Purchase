0c544fd274b2e181bf03d92edde64394e07b9140
Sarah Johnson <sarahrljohnson@gmail.com>
2018-11-23 15:13:13 +0100

    initial setup

---
+++ phone_purchase.js
@@ -0,0 +1,7 @@
+const taxRate = .06;
+const phonePrice = 250.00;
+const accessoryPrice = 24.00;
+const spendingThreshold = prompt("What is your phone and accessories budget?");
+const bankBalance = 800; 
+
+const totalPrice = function() {};
