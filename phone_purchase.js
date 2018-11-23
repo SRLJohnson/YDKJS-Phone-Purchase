4cceff64f661dbbe6fb590525683b57e5bc63add
Sarah Johnson <sarahrljohnson@gmail.com>
2018-11-23 15:26:19 +0100

    added withTax function

--- phone_purchase.js
+++ phone_purchase.js
@@ -1,7 +1,15 @@
 const taxRate = .06;
 const phonePrice = 250.00;
 const accessoryPrice = 24.00;
-const spendingThreshold = prompt("What is your phone and accessories budget?");
-const bankBalance = 800; 
+const spendingThreshold = 350.00;//input("What is your phone and accessories budget?");
+const bankBalance = 800;
+
+const withTax = function(itemPrice, taxRate) {
+    let taxAmount = itemPrice*taxRate;
+    let afterTaxes = itemPrice + taxAmount;
+    return afterTaxes.toFixed(2);
+}
 
 const totalPrice = function() {};
+
+console.log(withTax(phonePrice, taxRate));
